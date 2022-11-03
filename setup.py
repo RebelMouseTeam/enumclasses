636e01cf4235a50ef458c4bc5a6dbc07b3831a6e
cordis <cordis@game-mafia.ru>
2022-11-03 15:41:43 +0700

    Bump version: 1.1.3 → 1.1.4

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import setuptools
 
 setuptools.setup(
     name='enumclasses',
-    version='1.1.3',
-    author='Tomas Aparicio',
-    author_email='tomas@aparicio.me',
+    version='1.1.4',
+    author='CordiS',
+    author_email='cordis@rebelmouse.com',
     url='https://github.com/RebelMouseTeam/enumclasses',
     license='Apache 2.0',
     packages=['enumclasses'],
