59fa04a96f64fdbc4c70302a653c9667dc944816
cordis <cordis@game-mafia.ru>
2022-11-03 15:28:28 +0700

    + adding packaging-related files [TX-139]

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+setuptools.setup(
+    name='enumclasses',
+    version='1.1.1',
+    author='Tomas Aparicio',
+    author_email='tomas@aparicio.me',
+    url='https://github.com/RebelMouseTeam/enumclasses',
+    license='Apache 2.0',
+    packages=['enumclasses'],
+    test_suite='tests',
+    install_requires=[
+        'ply', 'decorator', 'six'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+)
